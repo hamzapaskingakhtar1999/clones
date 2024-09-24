fba4b2439f6462d83622139eea2f04173dfc503f
Hamza Pasking Akhtar <hamzapaskingakhtar@gmail.com>
2024-09-24 02:51:20 +0100

    Favicon added

--- Obys Agency/script.js
+++ Obys Agency/script.js
@@ -1,56 +1,60 @@
-var tl = gsap.timeline();
+function loadingAnimation() {
+  var tl = gsap.timeline();
 
-tl.from(".line h1", {
-  y: 150,
-  stagger: 0.25,
-  duration: 0.6,
-  delay: 0.5,
-});
+  tl.from(".line h1", {
+    y: 150,
+    stagger: 0.25,
+    duration: 0.6,
+    delay: 0.5,
+  });
 
-tl.from("#line1-part1, .line h2", {
-  opacity: 0,
-});
+  tl.from("#line1-part1, .line h2", {
+    opacity: 0,
+  });
 
-tl.to("#line1-part1", {
-  opacity: 1,
-  onStart: function () {
-    /* START */
-    var h5timer = document.querySelector("#line1-part1 h5");
-    var grow = 0;
+  tl.to("#line1-part1", {
+    opacity: 1,
+    onStart: function () {
+      /* START */
+      var h5timer = document.querySelector("#line1-part1 h5");
+      var grow = 0;
 
-    var counterInterval = setInterval(function () {
-      if (grow < 100) {
-        h5timer.innerHTML = grow++;
-      } else {
-        h5timer.innerHTML = grow;
-        clearInterval(counterInterval);
-      }
-      console.log(grow);
-    }, 25);
+      var counterInterval = setInterval(function () {
+        if (grow < 100) {
+          h5timer.innerHTML = grow++;
+        } else {
+          h5timer.innerHTML = grow;
+          clearInterval(counterInterval);
+        }
+        console.log(grow);
+      }, 25);
 
-    /* END */
-  },
-});
+      /* END */
+    },
+  });
 
-tl.to(".line h2", {
-  animationName: "anime",
-  opacity: 1,
-});
+  tl.to(".line h2", {
+    animationName: "anime",
+    opacity: 1,
+  });
 
-tl.to("#loader", {
-  opacity: 0,
-  duration: 0.2,
-  delay: 3,
-});
+  tl.to("#loader", {
+    opacity: 0,
+    duration: 0.2,
+    delay: 3,
+  });
 
-tl.from("#page1", {
-  delay: 0.2,
-  y: 1600,
-  opacity: 0,
-  ease: Power4,
-  duration: 0.5,
-});
+  tl.from("#page1", {
+    delay: 0.2,
+    y: 1600,
+    opacity: 0,
+    ease: Power4,
+    duration: 0.5,
+  });
 
-tl.to("#loader", {
-  display: "none",
-});
+  tl.to("#loader", {
+    display: "none",
+  });
+}
+
+loadingAnimation();
