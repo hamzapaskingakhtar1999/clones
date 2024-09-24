8267793b611f5fa862cfe55ac038c8384dd5ef1f
Hamza Pasking Akhtar <hamzapaskingakhtar@gmail.com>
2024-09-24 01:30:42 +0100

    Changed to timeline and re arranged JS

--- Obys Agency/script.js
+++ Obys Agency/script.js
@@ -1,19 +1,40 @@
-gsap.from(".line h1", {
+var tl = gsap.timeline();
+
+tl.from(".line h1", {
   y: 150,
   stagger: 0.25,
   duration: 0.6,
   delay: 0.5,
 });
 
-var h5timer = document.querySelector("#line1-part1 h5");
-var grow = 0;
+tl.from("#line1-part1, .line h2", {
+  opacity: 0,
+});
 
-var counterInterval = setInterval(function () {
-  if (grow < 100) {
-    h5timer.innerHTML = grow++;
-  } else {
-    h5timer.innerHTML = grow;
-    clearInterval(counterInterval);
-  }
-  console.log(grow);
-}, 25);
+tl.to("#line1-part1, .line h2", {
+  opacity: 1,
+  onStart: function () {
+    /* START - 
+Timer for the Loader */
+    var h5timer = document.querySelector("#line1-part1 h5");
+    var grow = 0;
+
+    var counterInterval = setInterval(function () {
+      if (grow < 100) {
+        h5timer.innerHTML = grow++;
+      } else {
+        h5timer.innerHTML = grow;
+        clearInterval(counterInterval);
+      }
+      console.log(grow);
+    }, 25);
+
+    /* END */
+  },
+});
+
+tl.to("#loader", {
+  opacity: 0,
+  duration: 0.4,
+  delay: 4,
+});
