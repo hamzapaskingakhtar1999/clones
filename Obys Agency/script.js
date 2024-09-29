bd4c2e7c9b7580fbfe643c688bd2318adc9775ba
Hamza Pasking Akhtar <hamzapaskingakhtar@gmail.com>
2024-09-29 02:17:19 +0100

    page 4 text and image

--- Obys Agency/script.js
+++ Obys Agency/script.js
@@ -60,8 +60,9 @@ function loadingAnimation() {
   });
 
   tl.from("#hero1 h1,#hero2 h1,#hero3 h2, #hero3 h3,#hero4 h1", {
-    y: 120,
+    y: 140,
     stagger: 0.2,
+    delay: 0.2,
   });
 }
 
@@ -80,5 +81,4 @@ function cursorAnimation() {
 }
 
 loadingAnimation();
-
 cursorAnimation();
